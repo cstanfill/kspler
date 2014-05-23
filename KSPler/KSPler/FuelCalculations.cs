5bd67c8269989a548f35039b0f3353f1da63348a
Colin Stanfill <colin.stanfill@gmail.com>
2014-05-23 14:11:29 -0400

    basic delta-v and orbital info ui

---
+++ KSPler/KSPler/FuelCalculations.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KSPler
+{
+    static class FuelCalculations
+    {
+        static IEnumerable<Part> filterByStage(IEnumerable<Part> lst, int stage) {
+            return (from p in lst where p.inverseStage == stage select p);
+        }
+
+        static IEnumerable<Part> filterByAbove(IEnumerable<Part> lst, int stage) {
+            return (from p in lst where p.inverseStage <= stage select p);
+        }
+
+        static IEnumerable<PartModule> getModules(IEnumerable<Part> lst) {
+            List<PartModule> res = new List<PartModule>();
+            foreach (Part p in lst) {
+                foreach (PartModule m in p.Modules) {
+                    res.Add(m);
+                }
+            }
+            return res.AsEnumerable();
+        }
+
+        static IEnumerable<T> filterSubtype<U, T>(IEnumerable<U> lst) where T : class {
+            return (from x in lst select (x as T)).Where((y) => y != null);
+        }
+
+        public static double getCurrentSurfaceWeight(IEnumerable<Part> lst) {
+            return lst.Sum(p => (p.mass + p.GetResourceMass()) * 9.81);
+        }
+
+        public static double getInitialMaxThrust(IEnumerable<Part> lst) {
+            return filterSubtype<PartModule, ModuleEngines>(getModules(filterByStage(lst, Staging.lastStage))).Sum(
+                (e) => e.maxThrust) +
+                filterSubtype<PartModule, ModuleEnginesFX>(getModules(filterByStage(lst, Staging.lastStage))).Sum(
+                (e) => e.maxThrust);
+        }
+
+        public static double getWetWeightAbove(IEnumerable<Part> lst, int stage) {
+            if (stage < 0) { return 0; }
+            return (filterByAbove(lst, stage)).Sum((p) => (p.mass + p.GetResourceMass()) * 9.81);
+        }
+
+        public static double getStageFuelWeight(IEnumerable<Part> lst, int stage) {
+            if (stage < 0) { return 0; }
+            return 0;
+        }
+
+        public static double getDryWeightAt(IEnumerable<Part> lst, int stage) {
+            if (stage < 0) { return 0; }
+            return (filterByStage(lst, stage)).Sum((p) => (p.mass * 9.81));
+        }
+
+        public static double getDryWeightAbove(IEnumerable<Part> lst, int stage) {
+            if (stage < 0) { return 0; }
+            return (filterByAbove(lst, stage)).Sum((p) => (p.mass) * 9.81);
+        }
+
+        public static double getWeightedIspAtAlt(IEnumerable<Part> lst, int stage, float alt) {
+            double ispsum = 0;
+            double tsum = 0;
+
+            foreach (ModuleEngines e in filterSubtype<PartModule, ModuleEngines>(getModules(filterByStage(lst, stage)))) {
+                tsum += e.maxThrust;
+                ispsum += e.maxThrust * e.atmosphereCurve.Evaluate(alt);
+            }
+
+            foreach (ModuleEnginesFX e in filterSubtype<PartModule, ModuleEnginesFX>(getModules(filterByStage(lst, stage)))) {
+                tsum += e.maxThrust;
+                ispsum += e.maxThrust * e.atmosphereCurve.Evaluate(alt);
+            }
+            if (ispsum == 0) { return 0; }
+            return ispsum / tsum;
+        }
+
+        public static double getWeightedIspVac(IEnumerable<Part> lst, int stage) {
+            return getWeightedIspAtAlt(lst, stage, 0);
+        }
+
+        public static double getWeightedIspAir(IEnumerable<Part> lst, int stage) {
+            return getWeightedIspAtAlt(lst, stage, 1000000000);
+        }
+
+        public static double deltaVAir(IEnumerable<Part> lst, int stage) {
+            var isp = getWeightedIspAir(lst, stage);
+            var m1 = getWetWeightAbove(lst, stage);
+            var m2 = getDryWeightAt(lst, stage) + getWetWeightAbove(lst, stage - 1);
+            var dv = isp * 9.81 * Math.Log(m1 / m2);
+            return getWeightedIspAir(lst, stage) * 9.81 * Math.Log(getWetWeightAbove(lst, stage) /
+                (getDryWeightAt(lst, stage) + getWetWeightAbove(lst, stage - 1)));
+        }
+
+
+        static void Print(string message) {
+            KSPlerInterface.print("[Fuel] " + message);
+        }
+
+        public static double deltaVVac(IEnumerable<Part> lst, int stage) {
+            var isp = getWeightedIspVac(lst, stage);
+            var m1  = getWetWeightAbove(lst, stage);
+            var m2  = getDryWeightAt(lst, stage) + getWetWeightAbove(lst, stage - 1);
+            var dv  = isp * 9.81 * Math.Log(m1/m2);
+            return dv;
+        }
+
+        public static double deltaVAir(IEnumerable<Part> lst) {
+            double sum = 0;
+            for (int i = 0; i <= Staging.lastStage; ++i) {
+                sum += deltaVAir(lst, i);
+            }
+            return sum;
+        }
+
+        public static double deltaVVac(IEnumerable<Part> lst) {
+            double sum = 0;
+            for (int i = 0; i <= Staging.lastStage; ++i) {
+                sum += deltaVVac(lst, i);
+            }
+            return sum;
+        }
+    }
+}
---
+++ KSPler/KSPler/KSPlerInterface.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+using fuel = KSPler.FuelCalculations;
+
+class KSPlerInterface : PartModule
+{
+    enum DisplayMode { EDITORMODE, FLYINGMODE, NONE }
+    DisplayMode current;
+    List<Callback> callbacks;
+    Rect area;
+
+    void switchMode(DisplayMode d) { 
+        current = d;
+        foreach (Callback i in callbacks) {
+            RenderingManager.RemoveFromPostDrawQueue(3, i);
+        }
+        if (d == DisplayMode.EDITORMODE) { 
+            area = new Rect((Screen.width/3) - 40, (Screen.height/3) - 40, 200, 200);
+            var c = new Callback(drawVehicleStats);
+            callbacks.Add(c);
+            RenderingManager.AddToPostDrawQueue(3, c);
+        }
+
+        if (d == DisplayMode.FLYINGMODE) {
+            Print("GUI Swithed to flight");
+            area = new Rect((Screen.width / 3) - 40, (Screen.height / 3) - 40, 200, 200);
+            var c = new Callback(drawOrbitStats);
+            callbacks.Add(c);
+            RenderingManager.AddToPostDrawQueue(3, c);
+        }
+    }
+
+    void drawOrbitStats() {
+        GUI.skin = HighLogic.Skin;
+        var w = GUILayout.Window(1, area, flightDVGUI, "Where Is To Be Goings", GUILayout.MinWidth(200));
+    }
+
+    void drawVehicleStats() {
+        GUI.skin = HighLogic.Skin;
+        var w = GUILayout.Window(1, area, vehicleGUI, "How Big To Be Makings", GUILayout.MinWidth(200));
+    }
+
+    void flightDVGUI(int windowID) {
+        Print("Flight DVGUI");
+        GUIStyle sty = new GUIStyle(GUI.skin.window);
+        sty.normal.textColor = sty.focused.textColor = Color.white;
+        sty.hover.textColor = sty.active.textColor = Color.gray;
+        sty.onNormal.textColor = sty.onFocused.textColor = sty.onActive.textColor = sty.onHover.textColor = Color.green;
+        sty.padding = new RectOffset(12, 12, 12, 12);
+
+        var lst = traverseChildren(this.vessel.rootPart);
+        Print("lst computed");
+
+        GUILayout.BeginVertical();
+        {
+            GUILayout.TextArea(String.Format("Apoapsis {0:N3}m",
+                this.vessel.orbit.ApR));
+            GUILayout.TextArea(String.Format("Periapsis; {0:N3}m",
+                this.vessel.orbit.PeR));
+        }
+        GUILayout.EndVertical();
+
+        GUILayout.BeginVertical();
+        {
+            GUILayout.TextArea(String.Format("Surface weight: {0:N3} kN",
+                fuel.getCurrentSurfaceWeight(lst)));
+            GUILayout.TextArea(String.Format("First stage thrust: {0:N3} kN",
+                fuel.getInitialMaxThrust(lst)));
+            GUILayout.TextArea(String.Format("Suface Thrust-weight ratio: {0:N3}",
+                fuel.getInitialMaxThrust(lst) / fuel.getCurrentSurfaceWeight(lst)));
+            GUILayout.TextArea(String.Format("Delta-V (Air, Vac): {0:N3}, {1:N3}",
+                fuel.deltaVAir(lst), fuel.deltaVVac(lst)));
+        }        
+        GUILayout.EndVertical();
+
+        GUI.DragWindow(new Rect(0, 0, 10000, 20));
+    }
+
+    IEnumerable<Part> traverseChildren(Part p) {
+        var desc = (from c in p.children select traverseChildren(c)).Aggregate((IEnumerable<Part>)(new LinkedList<Part>()), (a, b) => a.Concat(b));
+        return desc.Concat(new[] { p });
+    }
+
+    void vehicleGUI(int windowID) {
+        GUIStyle sty = new GUIStyle(GUI.skin.window);
+        sty.normal.textColor = sty.focused.textColor = Color.white;
+        sty.hover.textColor = sty.active.textColor = Color.gray;
+        sty.onNormal.textColor = sty.onFocused.textColor = sty.onActive.textColor = sty.onHover.textColor = Color.green;
+        sty.padding = new RectOffset(12, 12, 12, 12);
+
+        var lst = EditorLogic.SortedShipList;
+
+        GUILayout.BeginVertical();
+        {
+            GUILayout.TextArea(String.Format("Surface weight: {0:N3} kN",
+                fuel.getCurrentSurfaceWeight(lst)));
+            GUILayout.TextArea(String.Format("First stage thrust: {0:N3} kN",
+                fuel.getInitialMaxThrust(lst)));
+            GUILayout.TextArea(String.Format("Suface Thrust-weight ratio: {0:N3}",
+                fuel.getInitialMaxThrust(lst) / fuel.getCurrentSurfaceWeight(lst)));
+            GUILayout.TextArea(String.Format("Delta-V (Air, Vac): {0:N3}, {1:N3}",
+                fuel.deltaVAir(lst), fuel.deltaVVac(lst)));
+        }
+        GUILayout.EndVertical();
+
+        GUI.DragWindow(new Rect(0, 0, 10000, 20));
+    }
+
+    void Print(string message) {
+        print("[KSPler] " + message);
+    }
+
+    public override void OnStart(PartModule.StartState state) {
+        base.OnStart(state);
+        callbacks = new List<Callback>();
+        if (state == StartState.Editor) {
+            Print("Entering editor mode");
+            switchMode(DisplayMode.EDITORMODE);
+        } else if (state == StartState.Flying || state == StartState.PreLaunch
+            || state == StartState.Orbital || state == StartState.SubOrbital ||
+            state == StartState.Docked || state == StartState.Landed || state == StartState.Splashed || state == StartState.None) {
+            Print("Entering flight mode");
+            switchMode(DisplayMode.FLYINGMODE);
+        } else {
+            Print("Doing nothing. Not really, because I'm a fucking asshole hahahahahahahahahaha");
+            switchMode(DisplayMode.FLYINGMODE);
+        }
+    }
+
+    public override void OnInactive() {
+        base.OnInactive();
+        Print("Closing down.");
+        switchMode(DisplayMode.NONE);
+    }
+
+}
